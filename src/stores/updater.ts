969e119e0958d720103f53bd21788cb8c8d6de64
zhangkai <zhangkai@linyang.com.cn>
2026-08-24 13:24:25 +0800

    chore(release): bump version to 1.1.1

--- src/stores/updater.ts
+++ src/stores/updater.ts
@@ -40,7 +40,7 @@ export const useUpdaterStore = defineStore('updater', () => {
       try {
         currentVersion.value = await getApplicationVersion()
       } catch {
-        currentVersion.value = '1.1.0'
+        currentVersion.value = '1.1.1'
       }
 
       const update = await check()
